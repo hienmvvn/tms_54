baa00cc9543086482373ff80c48462d6017dbfe4
hien <hienmv.vn@gmail.com>
2016-05-10 10:19:08 +0700

    update profile

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,12 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   before_action :authenticate_user!
+  before_filter :configure_devise_permitted_parameters, if: :devise_controller?
+
+  protected
+  def configure_devise_permitted_parameters
+    devise_parameter_sanitizer.for(:account_update) do |user| 
+      user.permit :name, :email, :password, :current_password 
+    end
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,10 @@
 Rails.application.routes.draw do
-  devise_for :users
-  root "static_pages#home" 
+  devise_for :users, skip: [:registrations] 
+  as :user do
+    get "users/edit" => "devise/registrations#edit", as: "edit_user_registration"
+    patch "users" => "devise/registrations#update", as: "user_registration"
+  end
+  
+  root "static_pages#home"
+  resources :users, only: [:show]
 end
